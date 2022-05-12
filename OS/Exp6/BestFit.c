ff7b030ce33455c9d07b2a41777c23aab18b101a
cj-kid <chanderjindal2@gmail.com>
2022-05-12 17:15:28 +0530

    Memory Allotment, First, Best, Worst

---
+++ OS/Exp6/BestFit.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define Slots 50
+#define UpperMemoryLimit 200
+#define LowerMemoryLimit 50
+#define PrintSize 9
+
+struct Stack{
+    int TotalMemorySlots;
+    int MemorySlots[Slots];
+    int TotalProcesses;
+    int ProcessMemory[Slots];
+    int ResultCode;
+
+};
+
+void Generator(struct Stack* MyStack);
+void Printer(struct Stack MyStack);
+void MemoryAllocation(struct Stack* MyStack);
+
+int main(){
+struct Stack MyStack;
+Generator(&MyStack);
+Printer(MyStack);
+MemoryAllocation(&MyStack);
+Printer(MyStack);
+return 0;
+}
+
+void Generator(struct Stack* MyStack){
+    srand(time(0));
+
+    MyStack->TotalMemorySlots = rand()%Slots;
+    for(int i=0;i<MyStack->TotalMemorySlots;i++)
+        MyStack->MemorySlots[i] = rand()%UpperMemoryLimit + LowerMemoryLimit;
+
+    MyStack->TotalProcesses = rand()%MyStack->TotalMemorySlots;
+    for(int i =0;i<MyStack->TotalProcesses;i++)
+        MyStack->ProcessMemory[i] = rand()%LowerMemoryLimit;
+    
+    MyStack->ResultCode = 0;
+
+return ;
+}
+
+void Printer(struct Stack MyStack){
+
+    switch (MyStack.ResultCode){
+    case 0: printf("Newly initialized.\n");
+        break;
+    case -1: printf("Memory allotment is sucessfull!\n"); 
+    break;
+    default:
+    printf("Error at Process #%d.\n",MyStack.ResultCode);
+        break;
+    }
+
+    int temp = MyStack.TotalMemorySlots/PrintSize;
+    printf("The Set contains %d Memory Slots with following Capacity.\n",MyStack.TotalMemorySlots);
+        for(int i=0;i<((PrintSize > MyStack.TotalMemorySlots) ? MyStack.TotalMemorySlots : PrintSize);i++){
+        for(int j=0;j<=temp;j++){
+            int idx = j*PrintSize + i;
+            if(idx < MyStack.TotalMemorySlots)
+            printf(" %*d) -> %d Unit(s)",5,idx+1,MyStack.MemorySlots[idx]);
+        }
+        printf("\n");
+    }
+
+    printf("_________________________________________________\n");
+
+    temp = MyStack.TotalProcesses/PrintSize;
+    printf("The Set contains %d Processes with the following Memory requirements.\n",MyStack.TotalProcesses);
+
+    for(int i=0;i<((PrintSize > MyStack.TotalProcesses) ? MyStack.TotalProcesses : PrintSize);i++){
+        for(int j=0;j<=temp;j++){
+            int idx = j*PrintSize + i;
+            if(idx < MyStack.TotalProcesses)
+            printf(" %*d) -> %d Unit(s)",5,idx+1,MyStack.ProcessMemory[idx]);
+        }
+        printf("\n");
+    }
+
+    printf("\n----------------------------------------------\n");
+return;
+}
+
+void MemoryAllocation(struct Stack* MyStack){
+for(int i=0;i<MyStack->TotalProcesses;i++){
+    int MinMemory = UpperMemoryLimit,MinMemoryIdx = -1;
+        for(int j =0;j<MyStack->TotalMemorySlots;j++){
+            if(MyStack->MemorySlots[j] >= MyStack->ProcessMemory[i] && MyStack->MemorySlots[j] > -1){
+                if(MinMemory > MyStack->MemorySlots[j]){
+                    MinMemory = MyStack->MemorySlots[j];
+                    MinMemoryIdx = j;
+                }
+            }
+        }
+    if(MinMemoryIdx == -1){
+    MyStack->ResultCode = 1+i;
+    return;
+        }
+
+    MyStack->MemorySlots[MinMemoryIdx] = -(i+1);
+    MyStack->ProcessMemory[i] = 0;
+
+}
+
+MyStack->ResultCode = -1;
+return;
+}
---
+++ OS/Exp6/ReadMe.md
@@ -0,0 +1,7 @@
+Experiment 6
+
+Memory Fit 
+
+1)First - Pick the first adequate one
+2)Best - Pick the smallest adequate one
+3)Optimal - Pick the Largest one
